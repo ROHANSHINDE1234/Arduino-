3d1ae03982b8d52fa9797db783da6d4c9582a3ae
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2019-10-03 21:13:54 +0530

    Check this please

---
+++ simple_car/simple_car.ino
@@ -0,0 +1,16 @@
+int D0=7;
+int D1=8;
+int D2=9;
+int D3=10;
+void setup() 
+{
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  pinMode(D3,OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(D0,HIGH);
+}
