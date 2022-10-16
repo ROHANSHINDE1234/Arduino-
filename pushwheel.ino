5f0798f34ca65d723369cc36da24250439c2d63d
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-16 20:34:48 +0530

    Add files via upload

---
+++ pushwheel.ino
@@ -0,0 +1,30 @@
+#include <math.h>
+int p1 = 0;
+int p2 = 0;
+int p4 = 0;
+int p8 = 0;
+int a=0,b=0,c=0,d=0;
+void setup() {
+  pinMode(0,INPUT);
+  pinMode(1,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+
+void loop() {
+  p1 = digitalRead(0);
+  p2 = digitalRead(1);
+  p3 = digitalRead(2);
+  p4 = digitalRead(3);
+  if(p1 == HIGH)
+  {
+    a = 
+  }
+}
